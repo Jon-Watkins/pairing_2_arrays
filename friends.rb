9c028752e91b4ee995fd9ed18dc9444a9edcef07
Jon Watkins <jkwatkins85@gmail.com>
2023-02-08 15:34:54 -0700

    Initial commit

---
+++ friends.rb
@@ -0,0 +1,8 @@
+best_friend = "Charles"
+other_friend = "Chris"
+
+if best_friend == "Alex"
+    puts "Your best friend is #{best_friend}!"
+else
+    puts "Your best friend is #{other_friend}!"
+end
