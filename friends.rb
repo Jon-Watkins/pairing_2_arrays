8815b4241832f4685516f4e5289636f4a670b0b7
Jon Watkins <jkwatkins85@gmail.com>
2023-02-08 15:39:39 -0700

    Add other friend

--- friends.rb
+++ friends.rb
@@ -1,8 +1,11 @@
 best_friend = "Charles"
 other_friend = "Chris"
+good_friend = "Jesse"
 
 if best_friend == "Alex"
     puts "Your best friend is #{best_friend}!"
+elsif good_friend == "Jon"
+    puts "You've got a great friend named #{good_friend}."
 else
     puts "Your best friend is #{other_friend}!"
 end
